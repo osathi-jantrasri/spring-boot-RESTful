fb466bbbfb84c2465f2555e8720d3f6a29de2657
Osathi Jantrasri <otfor01@gmail.com>
2025-10-27 12:57:18 +0700

    init

---
+++ src/main/java/com/example/spring_boot_RESTful/SpringBootResTfulApplication.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_RESTful;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootResTfulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootResTfulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/spring_boot_RESTful/SpringBootResTfulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring_boot_RESTful;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootResTfulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
